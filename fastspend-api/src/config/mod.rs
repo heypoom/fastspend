c209523b74df419f51ecf8f87f4c7ded616e8adf
Phoomparin Mano <poom@hey.com>
2022-09-04 01:42:51 +0700

    refactor: extract keyword as sub-module

--- fastspend-api/src/config/mod.rs
+++ fastspend-api/src/config/mod.rs
@@ -1,37 +1,7 @@
+mod keyword;
+
+use keyword::{Keyword, KeywordType};
 use std::collections::HashMap;
-
-#[derive(PartialEq, Debug)]
-pub enum KeywordType {
-    CATEGORY,
-    PAYEE,
-}
-
-#[derive(PartialEq, Debug)]
-pub struct Keyword {
-    pub r#type: KeywordType,
-    pub key: String,
-    pub title: Option<String>,
-}
-
-impl Keyword {
-    pub fn into_transaction(&self) -> TransactionInfo {
-        match self.r#type {
-            KeywordType::CATEGORY => TransactionInfo {
-                payee: None,
-                category_id: Some(self.key.clone()),
-            },
-            KeywordType::PAYEE => TransactionInfo {
-                payee: Some(self.key.clone()),
-                category_id: None,
-            },
-        }
-    }
-}
-
-pub struct TransactionInfo {
-    pub payee: Option<String>,
-    pub category_id: Option<String>,
-}
 
 pub struct Config {
     pub keywords: HashMap<String, Keyword>,
@@ -69,10 +39,6 @@ impl Config {
     pub fn get_keyword(&self, alias: String) -> Option<&Keyword> {
         self.keywords.get(&alias)
     }
-
-    pub fn get_id_by_keyword(&self, alias: String) -> Option<String> {
-        self.get_keyword(alias).and_then(|k| Some(k.key.clone()))
-    }
 }
 
 static MOCK_CATEGORY: &'static str = "d3d92867-779b-453f-bf5b-0adf9859af96";
@@ -93,17 +59,12 @@ mod tests {
     fn it_gets_the_keyword() {
         let config = create_mock_config();
 
-        assert_eq!(
-            config.get_keyword("f".into()).unwrap().r#type,
-            KeywordType::CATEGORY
-        );
+        let keyword_f = config.get_keyword("f".into()).unwrap();
+        assert_eq!(keyword_f.r#type, KeywordType::CATEGORY);
+        assert_eq!(keyword_f.key, MOCK_CATEGORY);
 
-        assert_eq!(
-            config.get_keyword("sb".into()).unwrap().r#type,
-            KeywordType::PAYEE
-        );
-
-        assert_eq!(config.get_id_by_keyword("f".into()).unwrap(), MOCK_CATEGORY);
-        assert_eq!(config.get_id_by_keyword("sb".into()).unwrap(), "Starbucks");
+        let keyword_sb = config.get_keyword("sb".into()).unwrap();
+        assert_eq!(keyword_sb.r#type, KeywordType::PAYEE);
+        assert_eq!(keyword_sb.key, "Starbucks");
     }
 }
