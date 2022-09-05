76555c5a839c7f9b0ed51384fc0a1768f09e8d2e
Phoomparin Mano <poom@hey.com>
2022-09-06 00:30:48 +0700

    feat: add pwa plugin

--- fastspend-pwa/vite.config.ts
+++ fastspend-pwa/vite.config.ts
@@ -1,11 +1,12 @@
 import {defineConfig} from 'vite'
+import {VitePWA as pwa} from 'vite-plugin-pwa'
 import {svelte} from '@sveltejs/vite-plugin-svelte'
 
 import postcss from './postcss.config.cjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), pwa({registerType: 'autoUpdate'})],
 
   css: {postcss},
 })
