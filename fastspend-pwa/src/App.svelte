68adacc2748eb1611d0944032d712d46e58a2493
Phoomparin Mano <poom@hey.com>
2022-09-05 21:25:02 +0700

    chore: placeholder ui

--- fastspend-pwa/src/App.svelte
+++ fastspend-pwa/src/App.svelte
@@ -1,5 +1,7 @@
-<div class="bg-neutral-900">
+<div class="bg-stone-100">
   <div class="flex items-center justify-center min-h-screen">
-    <div class="text-5xl text-white">FastSpend UI</div>
+    <div class="px-5 py-3 shadow-xl bg-neutral-900 rounded-md">
+      <div class="text-4xl text-stone-100 font-extralight">FastSpend</div>
+    </div>
   </div>
 </div>
